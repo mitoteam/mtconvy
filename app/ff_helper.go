18b44c99a01a201028d382823f7a658bc2feb0ee
Mikhail Oleynik <mishutka@mito-team.com>
2023-03-23 05:02:49 +0300

    Showing streams

--- app/ff_helper.go
+++ app/ff_helper.go
@@ -1,26 +1,88 @@
 package app
 
 import (
+	"encoding/json"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/mitoteam/mttools"
 )
 
 type FfStream struct {
-	Name string
+	Index    int
+	Name     string
+	Language string
+	Data     jsonStream
+}
+
+type jsonFull struct {
+	Streams []jsonStream `json:"streams"`
+}
+
+type jsonStream struct {
+	Index       int               `json:"index"`
+	CodecName   string            `json:"codec_name"`
+	CodecType   string            `json:"codec_type"`
+	Width       int               `json:"width"`
+	Height      int               `json:"height"`
+	Disposition map[string]string `json:"disposition"`
+	Tags        map[string]string `json:"tags"`
 }
 
 func FfGetStreamList(path string) []FfStream {
 	args := make([]string, 0)
 	args = append(args, "-v", "quiet", "-print_format", "json", "-show_streams", path)
 
-	_, err := mttools.ExecCmd(AppSettings.FfprobePath, args)
-
+	json_str, err := mttools.ExecCmd(AppSettings.FfprobePath, args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	list := make([]FfStream, 0)
+	data := jsonFull{}
+
+	json.Unmarshal([]byte(json_str), &data)
+	//fmt.Println(data)
+
+	list := make([]FfStream, 0, len(data.Streams))
+
+	for _, streamData := range data.Streams {
+		stream := FfStream{
+			Index: streamData.Index,
+			Data:  streamData,
+		}
+
+		stream.Name = streamData.CodecType + "/" + streamData.CodecName
+
+		// resolution
+		if streamData.CodecType == "video" {
+			stream.Name += " " + strconv.Itoa(streamData.Width) + "x" + strconv.Itoa(streamData.Height)
+		}
+
+		//language
+		if streamData.CodecType == "audio" || streamData.CodecType == "subtitle" {
+			if v, exists := streamData.Tags["language"]; exists {
+				stream.Language = strings.ToUpper(v)
+
+				if stream.Language == "RU" {
+					stream.Language = "RUS"
+				} else if stream.Language == "EN" {
+					stream.Language = "ENG"
+				}
+
+				stream.Name += " " + stream.Language
+			}
+		}
+
+		//title
+		if streamData.CodecType == "audio" || streamData.CodecType == "subtitle" {
+			if v, exists := streamData.Tags["title"]; exists {
+				stream.Name += " " + v
+			}
+		}
+
+		list = append(list, stream)
+	}
 
 	return list
 }
--- app/task_item.go
+++ app/task_item.go
@@ -8,7 +8,19 @@ type TaskItem struct {
 }
 
 func (i TaskItem) SelectStreams() {
-	fmt.Printf("*** FILE: %s", i.Name)
+	fmt.Printf("*** FILE: %s\n", i.Name)
+	fmt.Println("Running ffprobe...")
 
-	FfGetStreamList(i.Path)
+	stream_list := FfGetStreamList(i.Path)
+
+	options_list := make([]string, 0, len(stream_list))
+	for _, stream := range stream_list {
+		options_list = append(options_list, stream.Name)
+	}
+
+	AskUserChoice(
+		"Select streams",
+		"Your choice: ",
+		options_list,
+	)
 }
